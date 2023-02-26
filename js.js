9ead0773f54476717037c357399345fe3a725b0e
hasanisokay <hasanisokay@gmail.com>
2023-02-26 17:32:41 +0600

    added details button

--- js.js
+++ js.js
@@ -21,23 +21,46 @@ const fetchingSearch = searchText => {
         .then(res => res.json())
         .then(data => displayFetchedData(data))
 }
+const cardContainer = document.getElementById('card-container');
 const displayFetchedData = data => {
+    cardContainer.innerHTML = ``;
     data.data.map(x => showInCard(x))
 }
 const showInCard = phone => {
-    console.log(phone)
-    const cardContainer = document.getElementById('card-container');
     const newDiv = document.createElement('div');
     newDiv.classList.add('card', 'col-4', 'g-4');
-    newDiv.style.width='18rem';
+    newDiv.style.width = '18rem';
     newDiv.innerHTML = `
         <img src="${phone.image}" class="card-img-top" alt="...">
         <div class="card-body">
             <h5 class="card-title">Brand: ${phone.brand}</h5>
             <h6 class="card-title">Model: ${phone.phone_name}</h6>
-            <p class="card-text">${phone.slug}</p>
-            <button class="btn btn-primary">Details</button>            
+            <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#phoneModal"  onclick="slugF('${phone.slug}')">Details</button>            
         </div>
     `
     cardContainer.appendChild(newDiv)
 }
+function slugF(x) {
+    fetch(`https://openapi.programming-hero.com/api/phone/${x}`)
+        .then(res => res.json())
+        .then(data => {
+            const slugInfo = data.data.mainFeatures;
+            console.log(slugInfo)
+            document.getElementById('phoneModalLabel').innerText = x;
+            document.getElementById('phone-storage').innerText = slugInfo.storage;
+            document.getElementById('phone-display').innerText = slugInfo.displaySize;
+            document.getElementById('phone-chipset').innerText = slugInfo.chipSet;
+            document.getElementById('phone-memory').innerText = slugInfo.memory;
+            const sensors = document.getElementById('phone-sensors');
+            slugInfo.sensors.map(sensor=> {
+                const newSpan = document.createElement('span');
+                newSpan.innerText = sensor +", ";
+                sensors.appendChild(newSpan)
+            })
+        })
+}
+// phone-display"></span>></p>
+{/* <p>Storage <span id="phone-storage"></span>></p> */ }
+{/* <p>Chipset <span id="phone-chipset"></span>></p> */ }
+{/* <p>Memory <span id="phone-memory" */ }
+document.getElementById
